faef8e98bc92a4a631856b7d499e2f19f85da061
clovisdasilvaneto <cloves13@hotmail.com>
2014-11-10 16:04:47 -0300

    add css

---
+++ offer/migrations/0007_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('offer', '0006_auto_20141107_1732'),
+        ('offer', '0006_auto_20141107_1448'),
+    ]
+
+    operations = [
+    ]
